59ee2d4e0d7e573a960b8c2b504bd505c15ef795
sabamontazeri <sabaabaszade3@gmail.com>
2022-01-04 13:16:31 +0330

    withoght log

--- main.py
+++ main.py
@@ -6,10 +6,7 @@ import os
 
 app = Flask(__name__)
 
-logging.basicConfig(
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
-)
-logger = logging.getLogger(__name__)
+
 
 
 def start(update: Update, context: CallbackContext) -> None:
