33d931cd71413a9b51744b9aa7bf8cdd847874db
sabamontazeri <sabaabaszade3@gmail.com>
2022-01-13 19:00:01 +0330

    selenium

--- main.py
+++ main.py
@@ -6,8 +6,10 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import re
+import os
+mylist=''
 karbar=[]
-first,second=range(2)
+first,second,third=range(3)
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
 )
@@ -20,58 +22,73 @@ def start(update: Update, context: CallbackContext) -> None:
     update.message.reply_text(text=bot_welcome)
     keyboard = [
         [
-            InlineKeyboardButton("سربال ", callback_data='series')
+            InlineKeyboardButton("سریال ", callback_data='series')
         ],
         [InlineKeyboardButton("فیلم", callback_data='movie')]
     ]
 
     reply_markup = InlineKeyboardMarkup(keyboard)
 
-    update.message.reply_text('فیلم یا سریال؟', reply_markup=reply_markup)
-    query = update.callback_query
-    query.answer()
-    query.edit_message_text(text="انتخاب شد!")
-    karbar.append(query.data)
+    update.message.reply_text('فیلم یا سریال؟🎥', reply_markup=reply_markup)
 
     return first
 
 def country(update: Update, context: CallbackContext):
-    keyboard=[[
-            InlineKeyboardButton("ایران🇮🇷 ", callback_data='53'),
-            InlineKeyboardButton("آمریکا", callback_data='54'),
-        ],
-        [InlineKeyboardButton("هند", callback_data='59'),
-         InlineKeyboardButton("سایر", callback_data='63')],]
-    reply_markup = InlineKeyboardMarkup(keyboard)
-    update.message.reply_text('کشور مورد نظر خود را انتخاب کنید:', reply_markup=reply_markup)
-
     query = update.callback_query
     query.answer()
-    query.edit_message_text(text="کشور انتخاب شد!")
+    keyboard=[[
+            InlineKeyboardButton("ایران🇮🇷 ", callback_data='53'),
+            InlineKeyboardButton("آمریکا🇱🇷", callback_data='54'),
+        ],
+        [InlineKeyboardButton("هند🇮🇳", callback_data='59'),
+         InlineKeyboardButton("سایر🌐", callback_data='63')],]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+
+
+    query.edit_message_text(text="به سینمای کدام کشور علاقه دارید؟",reply_markup=reply_markup)
     karbar.append(query.data)
     return second
 
 def theme(update: Update, context: CallbackContext) -> None:
-    keyboard = [
-        [
-            InlineKeyboardButton("کمدی", callback_data='comedy'),
-            InlineKeyboardButton("اکشن", callback_data='action'),
-        ],
-        [InlineKeyboardButton("ترسناک", callback_data='horror'),InlineKeyboardButton("درام",callback_data='dramatic')],
-    ]
-    reply_markup = InlineKeyboardMarkup(keyboard)
-    update.message.reply_text('کشور مورد نظر خود را انتخاب کنید:', reply_markup=reply_markup)
-
     query = update.callback_query
     query.answer()
-    query.edit_message_text(text="کشور انتخاب شد!")
+    keyboard = [
+        [
+            InlineKeyboardButton("کمدی", callback_data='کمدی'),
+            InlineKeyboardButton("اکشن", callback_data='اکشن'),
+        ],
+        [InlineKeyboardButton("ترسناک", callback_data='ترسناک'),InlineKeyboardButton("درام",callback_data='درام')],
+        [InlineKeyboardButton("جنگی", callback_data='جنگی'),InlineKeyboardButton("عاشقانه",callback_data='عاشقانه')],
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+
+    query.edit_message_text(text="به کدام ژانر علاقه دارید؟🎭",reply_markup=reply_markup)
     karbar.append(query.data)
+    return third
 
 def end(update: Update, context: CallbackContext):
+    query = update.callback_query
+    query.answer()
+    karbar.append(query.data)
+    update.callback_query.message.edit_text(text="ژانر مورد نظر شما انتخاب شد")
 
-    for i in karbar:
-        mylist+=f'{i} \n'
-    update.message.reply_text(text=mylist)
+def inf(update: Update, context: CallbackContext) -> None:
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--no-sandbox")
+    driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+    driver.get(f'https://www.namava.ir/search?type={karbar[0]}&country={karbar[1]}&genre={karbar[2]}')
+
+    movienames = []
+    elements = driver.find_elements(By.TAG_NAME, 'img')
+
+    for element in elements:
+        movienames.append(element.get_attribute('title').strip())
+    for i in movienames:
+        text+=f'{i}\n'
+    update.message.reply_text(text=text)
 
 
 
@@ -86,17 +103,24 @@ def main() -> None:
     conv_handler=ConversationHandler(
         entry_points=[CommandHandler('start',start)],
         states={first:[CallbackQueryHandler(country,pattern='^' + 'series' + '$'),
-                 CallbackQueryHandler(country,pattern='^' + 'film' + '$')],
+                 CallbackQueryHandler(country,pattern='^' + 'movie' + '$')],
                  second:[CallbackQueryHandler(theme,pattern='^' + '53' + '$'),
                          CallbackQueryHandler(theme,pattern='^' + '54' + '$'),
                          CallbackQueryHandler(theme,pattern='^' + '59' + '$'),
-                         CallbackQueryHandler(theme,pattern='^' + '63' + '$')]
+                         CallbackQueryHandler(theme,pattern='^' + '63' + '$')],
+                 third:[CallbackQueryHandler(end,'^' + 'کمدی' + '$'),
+                        CallbackQueryHandler(end,'^' + 'ترسناک' + '$'),
+                        CallbackQueryHandler(end,'^' + 'جنگی' + '$'),
+                        CallbackQueryHandler(end,'^' + 'درام' + '$'),
+                        CallbackQueryHandler(end,'^' + 'عاشقانه' + '$'),
+                        CallbackQueryHandler(end,'^' + 'اکشن' + '$')]
 
         },
     fallbacks = [CommandHandler('start', start)]
     )
     dispatcher.add_handler(conv_handler)
     updater.dispatcher.add_handler(CommandHandler('end', end))
+    updater.dispatcher.add_handler(CommandHandler('inf', inf))
 
     # Start the Bot
     updater.start_polling()
