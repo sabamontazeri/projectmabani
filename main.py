afec7557cf7d422bac611a4657a234a45abb5816
alidavood45 <alidavoodi2026@gmail.com>
2022-01-21 01:06:13 -0800

    third commit

--- main.py
+++ main.py
@@ -1,68 +1,24 @@
-import json
-import requests
-import os
+from selenium import webdriver
+from selenium.webdriver.common.by import By
 
-url = 'https://api.telegram.org/bot5053445626:AAFqMUVL1DhaQ4gUncS7PqqGDlheJQs6HiI/'
+karbar = input().split()
+driver = webdriver.Firefox(executable_path='C:\\Users\\Sportg\\Desktop\\geckodriver.exe')
+driver.get(f'https://www.namava.ir/search?type={karbar[0]}&country={karbar[1]}&genre={karbar[2]}')
 
-from flask import Flask
-from flask import request
-from flask import Response
+movienames = []
+film = input()
+elements = driver.find_elements(By.TAG_NAME, 'img')
 
-app = Flask(__name__)
-def get_all_updates():
-    response = requests.get(url + 'getupdates')
-    return response.json()
-
-def get_last_update(allupdates):
-    return allupdates['result'][-1]
-
-def get_chat_id(update):
-    return update['message']['chat']['id']
-
-def sendmessage(chat_id, text):
-    senddata = {'chat_id': chat_id, 'text': text}
-    response = requests.post(url + 'sendmessage', senddata)
-    return response
+for element in elements:
+    movienames.append(element.get_attribute('title'))
+print(movienames[:20])
 
 
-@app.route('/', methods=['POST', 'GET'])
-def index():
-    msg = request.get_json()
-    if request.method == 'POST':
-        msg = request.get_json()
-        chat_id = get_chat_id(msg)
-        text = msg['message'].get('text', '')
-        if text == '/start':
-            sendmessage(chat_id, 'khosh amadid')
-        elif 'new' in text:
-            contacts = read_json()
-            username = msg['message']['from']['username']
-            if username not in contacts.keys():
-                contacts[username] = []
-            mokhatab = text.split(maxsplit=1)[1]
-            contacts[username].append(mokhatab)
-            write_json(contacts)
-        elif text == 'list':
-            contacts = read_json()
-            username = msg['message']['from']['username']
-            if username not in contacts.keys():
-                sendmessage(chat_id, 'shoma mokhatabi ndarid')
-            else:
-                for mokhatab in contacts[username]:
-                    sendmessage(chat_id, mokhatab)
-        return Response('ok', status=200)
-    else:
-        return '<h1>salam</h1>'
-
-def write_json(data, filename='contactlist.json'):
-    with open(filename, 'w') as target:
-        json.dump(data, target, indent=4, ensure_ascii=False)
-
-def read_json(filename='contactlist.json'):
-    with open(filename, 'r') as target:
-        data = json.load(target)
-    return data
-
-
-write_json({})
-app.run(host='0.0.0.0',port=int(os.environ.get('PORT', 5000)))
+everylinks = []
+def linkfilm(driver, everylinks):
+    links = driver.find_elements(By.TAG_NAME, 'a')
+    for item in links:
+        eachlink = item.get_attribute('href')
+        everylinks.append(eachlink)
+    return everylinks
+links = linkfilm(driver, everylinks)
